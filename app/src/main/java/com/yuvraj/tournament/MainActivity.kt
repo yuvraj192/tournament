73352bc3b903531e6a89223667d742fe455c4c74
divyansh.gupta <divyanshg809@gmail.com>
2020-07-04 23:54:05 +0530

    test

--- app/src/main/java/com/yuvraj/tournament/MainActivity.kt
+++ app/src/main/java/com/yuvraj/tournament/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.yuvraj.tournament
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.JavascriptInterface
 import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
